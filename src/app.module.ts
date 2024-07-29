e4120b1e7f4fc9746caa13900d601c55ff9c2aa2
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-07-29 10:52:32 -0500

    Module definition with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersistenceModule } from './persistence/persistence.module';
+import { ConfigModule } from '@nestjs/config';
+import dbConfig from './persistence/db-config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      load: [dbConfig],
+      isGlobal: true,
+    }),
+    PersistenceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
