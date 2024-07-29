6cba3628becbc3e6cedf09d0ab9f5c7578181dc2
daniejl04 <danielestebanjimenezlopez@gmail.com>
2024-07-29 11:30:49 -0500

    add entity

--- src/upload-file/entities/upload-file.entity.ts
+++ src/upload-file/entities/upload-file.entity.ts
@@ -1,1 +1,33 @@
-export class UploadFile {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+import * as mongoose from "mongoose";
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
+@Schema()
+export class UploadFileLog extends Document {
+
+  @Prop({ unique: true })
+  id: number;
+
+  @Prop({ required: true })
+  filename: string;
+
+  @Prop({ required: true })
+  totalRecords: number;
+
+  @Prop({ required: true })
+  processedRecords: number;
+
+  @Prop({ required: true })
+  timestamp: Date;
+
+  @Prop({ required: true, unique: true })
+  fileHash: string;
+}
+
+const UploadFileLogSchema = SchemaFactory.createForClass(UploadFileLog);
+
+UploadFileLogSchema.plugin(AutoIncrement, { inc_field: 'id' });
+
+export { UploadFileLogSchema };
+
