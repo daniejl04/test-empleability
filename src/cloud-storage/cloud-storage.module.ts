d774c2ceb23af1ddcfbe50a358024c11d8ff6249
Andres Gomez <pipe2893@gmail.com>
2024-07-29 13:00:53 -0500

    implementation of this module

--- src/cloud-storage/cloud-storage.module.ts
+++ src/cloud-storage/cloud-storage.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CloudStorageService } from './cloud-storage.service';
-import { CloudStorageController } from './cloud-storage.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  controllers: [CloudStorageController],
+  imports:[ConfigModule.forRoot()],
+  controllers: [],
   providers: [CloudStorageService],
 })
 export class CloudStorageModule {}
