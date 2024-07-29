fcc6f2f3c1e6578fd86e9c40dc4fc6e0b1152ff0
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-07-29 13:13:16 -0500

    add commet for logsMpdule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadFileModule } from './upload-file/upload-file.module';
-import { LogsModule } from './logs/logs.module';
+//import { LogsModule } from './logs/logs.module';
 import { PersistenceModule } from './persistence/persistence.module';
 import { ConfigModule } from '@nestjs/config';
 import { CloudStorageModule } from './cloud-storage/cloud-storage.module';
@@ -18,7 +18,7 @@ import dbConfig from './persistence/db-config';
     }),
     PersistenceModule,
     UploadFileModule, 
-    LogsModule, 
+    //LogsModule, 
     CloudStorageModule
   ],
   controllers: [AppController],
