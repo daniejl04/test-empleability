533ade9dc8f5b0f8f49b13f44a399d2e390c3c5b
alexanderhernandez17 <alexander.hdez.2001@gmail.com>
2024-07-29 10:54:25 -0500

    fixed log entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UploadFileModule } from './upload-file/upload-file.module';
+import { LogsModule } from './logs/logs.module';
 
 @Module({
-  imports: [UploadFileModule],
+  imports: [UploadFileModule, LogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
