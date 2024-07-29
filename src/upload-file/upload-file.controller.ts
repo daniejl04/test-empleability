d5692da312d9bb8679f7682fa4d8a4e7554c8601
alexanderhernandez17 <alexander.hdez.2001@gmail.com>
2024-07-29 10:21:26 -0500

    created upload file module

---
+++ src/upload-file/upload-file.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UploadFileService } from './upload-file.service';
+import { CreateUploadFileDto } from './dto/create-upload-file.dto';
+import { UpdateUploadFileDto } from './dto/update-upload-file.dto';
+
+@Controller('upload-file')
+export class UploadFileController {
+  constructor(private readonly uploadFileService: UploadFileService) {}
+
+  @Post()
+  create(@Body() createUploadFileDto: CreateUploadFileDto) {
+    return this.uploadFileService.create(createUploadFileDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.uploadFileService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.uploadFileService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUploadFileDto: UpdateUploadFileDto) {
+    return this.uploadFileService.update(+id, updateUploadFileDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.uploadFileService.remove(+id);
+  }
+}
---
+++ src/upload-file/upload-file.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUploadFileDto } from './dto/create-upload-file.dto';
+import { UpdateUploadFileDto } from './dto/update-upload-file.dto';
+
+@Injectable()
+export class UploadFileService {
+  create(createUploadFileDto: CreateUploadFileDto) {
+    return 'This action adds a new uploadFile';
+  }
+
+  findAll() {
+    return `This action returns all uploadFile`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} uploadFile`;
+  }
+
+  update(id: number, updateUploadFileDto: UpdateUploadFileDto) {
+    return `This action updates a #${id} uploadFile`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} uploadFile`;
+  }
+}
